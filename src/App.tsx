e107973118010355043db83c38da76f5cba34e23
Daniel Harrison <daniel.harrison.lima@gmail.com>
2024-12-15 17:55:25 -0300

    feat: add layots and react-helmete-async

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import { RouterProvider } from 'react-router'
 import { Button } from './components/ui/button'
 import './index.css'
 import { router } from './routes'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 
 export function App() {
   return (
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+          <RouterProvider router={router} />
+    </HelmetProvider>
+
   )
 }
 
